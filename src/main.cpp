c4875183c84362bc36230952e27d30e287bd2b4f
someone3332 <leo.morat@epitech.eu>
2026-01-31 11:06:08 +0100

    [FIX] fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv){
     if (argc > 1 && (std::string(argv[1]) == "-gui")) {   
         Display::Sfml a;
         a.init();
-    } else if (argv[1] == 's') {
+    } else {
         Display::Ncurses a;
         a.init();
         a.newSection("q");
